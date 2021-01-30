7588575caf36a2a8ecda892fc5bdae9247258d20
SoloProger <andreisolo.lite@gmail.com>
2021-01-31 00:57:13 +0300

    New Update

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+const path = require('path')
+
+const PORT = process.env.PORT || 8080
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(todoRoutes)
+
+async function start() {
+    try {
+        await mongoose.connect(
+            'mongodb+srv://admin:1q2w3e4r@cluster0.6xkud.mongodb.net/todos?retryWrites=true&w=majority', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        })
+    } catch (e){
+        console.log(e)
+    }
+}
+
+start()
